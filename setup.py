bdfdb54ee26f8cf2e1ca71390c5a914f041d889b
JacobTh98 <jacob.thoenes@googlemail.com>
2023-02-13 14:08:23 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="jatools",
+    version="0.1",
+    author="Jacob Peter Thönes",
+    author_email="jacob.thoenes@uni-rostock.de",
+    description="Python packages that are useful during the work at the university of Rostock",
+    #long_description=open("README.md").read(),
+    #long_description_content_type="text/markdown",
+    #keywords="ScioSpec EIT".split(),
+    platforms="any",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    url="https://github.com/spatialaudio/sciopy.git",
+)

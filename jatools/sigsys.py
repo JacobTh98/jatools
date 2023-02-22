fa8c168d1eec87a77ca177f6b8844fa77373963f
JacobTh98 <jacob.thoenes@googlemail.com>
2023-02-22 15:41:27 +0100

    added normalization function

--- jatools/sigsys.py
+++ jatools/sigsys.py
@@ -18,3 +18,31 @@ def circ_conv(sign_1: np.ndarray, sign_2: np.ndarray) -> np.ndarray:
         Circular convolution
     """
     return np.real(np.fft.ifft(np.fft.fft(sign_1) * np.fft.fft(sign_2)))
+
+
+def norm_data(data: np.ndarray, low_bound: int = 0, high_bound: int = 1) -> np.ndarray:
+    """
+    Normalise data to a given boundary. If the data is complex the absolute value ist computed.
+
+    Parameters
+    ----------
+    data : np.ndarray
+        data
+    low_bound : int, optional
+        lower boundary, by default 0
+    high_bound : int, optional
+        above boundary, by default 1
+
+    Returns
+    -------
+    np.ndarray
+        absolute and normalized data
+    """
+
+    norm_data = []
+    diff = high_bound - low_bound
+    diff_arr = max(data) - min(data)
+    for i in data:
+        temp = (((i - min(data)) * diff) / diff_arr) + low_bound
+        norm_data.append(temp)
+    return np.array(norm_data)

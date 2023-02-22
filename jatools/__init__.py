2d19309638c827df4545d8c6881e1a08f1f986c3
JacobTh98 <jacob.thoenes@googlemail.com>
2023-02-22 15:41:29 +0100

    added normalization function

--- jatools/__init__.py
+++ jatools/__init__.py
@@ -2,6 +2,7 @@ from .mathtex import print_matrix
 from .maths import p_q_solve
 from .plotting import plot_signal, plot_sin_cos, z_plane_plot
 from .decorators import logger, cache, repeat, timeit, retry, countcall, call_api
+from sigsys import circ_conv, norm_data
 
 __all__ = [
     # mathtex
@@ -20,4 +21,7 @@ __all__ = [
     retry,
     countcall,
     call_api,
+    # sigsys
+    circ_conv,
+    norm_data,
 ]
